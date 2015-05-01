4223c335dfe33c6ac09db1b8e29171445b5987ba
Johnny Awesome <johnslayer1981@gmail.com>
2015-04-30 18:36:55 -0700

    Added some UIView elements for each containter

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var firstContainer: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fourthContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
